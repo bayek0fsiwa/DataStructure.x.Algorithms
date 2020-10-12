ab5d32de3dcf73194eea143bef8b088df6709bfb
bayek0fsiwa <imvikas@tutanota.com>
2020-10-12 18:06:25 +0530

    fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++){
+        if (i % 3 === 0){
+            console.log("fizz");
+        } else if (i % 5 === 0) {
+                console.log("buzz");
+            } else if (i % 3 && i % 5 === 0){
+                    console.log("fizzbuzz");
+                
+            }
+        }
+    }
+
+let num = fizzBuzz(15)
+console.log(num);
