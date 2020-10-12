04f3fe2097ec7c585c7d99a596e0fef6863e527a
bayek0fsiwa <imvikas@tutanota.com>
2020-10-12 21:31:20 +0530

    fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,14 @@
 function fizzBuzz(n) {
-    for (let i = 1; i <= n; i++){
-        if (i % 3 === 0){
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0) {
             console.log("fizz");
         } else if (i % 5 === 0) {
-                console.log("buzz");
-            } else if (i % 3 && i % 5 === 0){
-                    console.log("fizzbuzz");
-                
-            }
+            console.log("buzz");
+        } else if (i % 3 && i % 5 === 0) {
+            console.log("fizzbuzz");
         }
     }
+}
 
-let num = fizzBuzz(15)
+let num = fizzBuzz(100)
 console.log(num);
---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+def fizzBuzz(n):
+    for i in range(1, n+1):
+        if i % 3 == 0:
+            print('fizz')
+        elif i % 5 == 0:
+            print('buzz')
+        elif i % 3 == 0 and i % 5 == 0:
+            print('fizzbuzz')
+fizzBuzz(100)
