6ee787dc0bd97eabc9cefe5e3dac07858b9b7d0e
bayek0fsiwa <imvikas@tutanota.com>
2020-10-15 15:07:53 +0530

    sqrt

---
+++ sqroot.py
@@ -0,0 +1,7 @@
+# Python Program to Find the Square Root without importing math module
+# But it will only work for positive numbers
+number = int(input("Enter any Positive Number: "))
+
+srt = number ** 0.5
+
+print(srt)
