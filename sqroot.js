d707f2607b43905ae1830ef3f91369d04480b7cb
bayek0fsiwa <imvikas@tutanota.com>
2020-10-15 15:18:26 +0530

    sqrtjs

---
+++ sqroot.js
@@ -0,0 +1,5 @@
+// Javascript Program to Find the Square Root without importing math module
+// But it will only work for positive numbers
+const number = 2
+let result = number ** 0.5
+console.log(result);
