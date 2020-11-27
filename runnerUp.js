46b6fae655156d29044c4024dea0c37c58dd7efd
bayek0fsiwa <imvikas@tutanota.com>
2020-11-27 20:01:52 +0530

    runnerUp

--- runnerUp.js
+++
@@ -1,4 +0,0 @@
-// let number = 5
-arr = [5, 3, 4, 1, 2]
-sortedArr = arr.sort().reverse()
-console.log(sortedArr[1])
---
+++ runnerUp.ts
@@ -0,0 +1,3 @@
+let arr = [5, 3, 4, 1, 2]
+let sortedArr = arr.sort().reverse()
+console.log(sortedArr[1])
