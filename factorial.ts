4d06e1176119538a55bd5f9aeac1a2b0e4845b11
bayek0fsiwa <imvikas@tutanota.com>
2020-12-16 22:07:26 +0530

    factorial

--- factorial.ts
+++ factorial.ts
@@ -1,6 +1,16 @@
-function factorial(x: number) {
+// Recursive Function
+function factorial(x: number){
     if(x === 0){
         return 1
     }
     return x * factorial(x - 1)
+}
+
+// Iterative Function 
+function fact(num: number){
+    let val = 1
+    for(let i = 2; i <= num; i++){
+        val *= i
+        return val
+    }
 }
